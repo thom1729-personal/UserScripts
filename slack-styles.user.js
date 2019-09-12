977c9673faa4ca9ea18b72106dc837f872016a40
Thomas Smith <thom@thomthesmith.com>
2019-09-12 14:16:59 -0400

    Add slack styles.

---
+++ slack-styles.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Slack dark mode tweaks
+// @match        https://app.slack.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const styleElement = document.createElement('style');
+    styleElement.textContent = `
+    .sk-client-theme--dark {
+        --sk_primary_background: 39,44,49;
+        --sk_inverted_foreground: 39,44,49;
+    }`;
+    document.head.appendChild(styleElement);
+})();
